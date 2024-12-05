f1eccda4c036c2fdff117eabe5b1fbef4b7b05c0
Pierre LEDAC <pierre.ledac@cea.fr>
2024-12-05 16:43:44 +0100

    [Docker] Add push to public docker hub, and how to save image to tgz format.

--- env_src/docker/docker.sh
+++ env_src/docker/docker.sh
@@ -5,7 +5,24 @@ TRUST_ROOT=`pwd`/../../
 # Build the container to build TRUST on a specific OS
 docker build --rm --build-arg MY_OS=$MY_OS -t my_$MY_OS .
 docker run -v $TRUST_ROOT:/home/trust my_$MY_OS
+
 # Build the container with the binary for the specific OS
 cp ../../exec/TRUST_mpi_opt .
 docker build --rm --build-arg MY_OS=$MY_OS -f Dockerfile2 -t trust_$MY_OS .
+rm -f ./TRUST_mpi_opt
+docker run trust_$MY_OS .
+
+# Changement de / et : en _ pour le nom des fichiers:
+MY_OS=`echo $MY_OS | awk '{gsub("\\\/","_",$0);gsub(":","_",$0);print $0}'`
+
+# Container pushed on hub.docker.com:
+tag="pledac/"trust_$MY_OS
+docker tag trust_$MY_OS $tag
+docker push $tag
+
+# Docker image in a compressed format:
+save=$TRUST_ROOT/trust_"$MY_OS".tar.gz
+docker save trust_$MY_OS | gzip > $save
+echo "$save created."
+
 
