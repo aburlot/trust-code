5ddd01aa5220d6add6b88c3a293d1d9ee2c29b2d
Pierre LEDAC <pierre.ledac@cea.fr>
2024-12-05 11:49:41 +0100

    [Docker] adduser pour etre non root dans le container + construction du container de livraison

--- env_src/docker/docker.sh
+++ env_src/docker/docker.sh
@@ -1,4 +1,11 @@
 # Pour eviter de creer des images multiples: --rm
 MY_OS=redhat/ubi8
 #MY_OS=fedora:41
-docker build --rm --build-arg MY_OS=$MY_OS -t my_$MY_OS . && docker run -v /home/formation/s-sac-dm2s-train9/fedora/trust-code:/tmp my_$MY_OS
+TRUST_ROOT=`pwd`/../../
+# Build the container to build TRUST on a specific OS
+docker build --rm --build-arg MY_OS=$MY_OS -t my_$MY_OS .
+docker run -v $TRUST_ROOT:/home/trust my_$MY_OS
+# Build the container with the binary for the specific OS
+cp ../../exec/TRUST_mpi_opt .
+docker build --rm --build-arg MY_OS=$MY_OS -f Dockerfile2 -t trust_$MY_OS .
+
