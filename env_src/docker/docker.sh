a673593aab8e308e66ad3a5bfaa14640f1661ef3
Pierre LEDAC <pierre.ledac@cea.fr>
2024-12-06 16:41:18 +0100

    [Docker] Update

--- env_src/docker/docker.sh
+++ env_src/docker/docker.sh
@@ -2,31 +2,34 @@
 MY_OS=rockylinux:8
 #MY_OS=redhat/ubi8 # N'a pas openmpi...
 #MY_OS=fedora:41
-TRUST_ROOT=`pwd`/../../
-exec=$TRUST_ROOT/exec/TRUST_mpi_opt
-# Build the container to build TRUST on a specific OS
-docker build --rm --build-arg MY_OS=$MY_OS -t my_$MY_OS .
 
-docker run -v $TRUST_ROOT:/home/trust my_$MY_OS `[ -f $exec ] && echo "Binary $exec ready."`
+# Nom de l'image
+image=pledac/`echo trust-$MY_OS | awk '{gsub("\\\/","-",$0);gsub(":","-",$0);print $0}'`
+TRUST_ROOT=`pwd`/../..
+exec=$TRUST_ROOT/exec/TRUST_mpi_opt
+
+# Build the container to build TRUST on a specific OS
+docker build --rm --build-arg MY_OS=$MY_OS -t $image .
+
+# Run the container 
+docker run -v $TRUST_ROOT:/home/trust $image `[ -f $exec ] && echo "Binary $exec ready."`
 
 # Build the container with the binary for the specific OS
-image=trust_$MY_OS
 cp $exec .
 docker build --rm --build-arg MY_OS=$MY_OS -f Dockerfile2 -t $image .
 rm -f ./TRUST_mpi_opt
-touch toto.data
-docker run $image toto . || exit -1
 
-# Changement de / et : en _ pour le nom des fichiers:
-name_image=`echo $image | awk '{gsub("\\\/","_",$0);gsub(":","_",$0);print $0}'`
+# Test the container locally with a data file:
+echo "system \"echo Docker works\"" > dumb.data
+docker run -v `pwd`:/tmp $image dumb . || exit -1
+rm -f dumb.data
 
-# Container pushed on hub.docker.com:
-tag=pledac/$name_image
-docker tag $image $tag 	|| exit -1
-docker push $tag	|| exit -1
 
-# Docker image in a compressed format:
-save=$TRUST_ROOT/$name_image.tar.gz
+# Container pushed on hub.docker.com: Le tag pourrait etre plus tot...
+docker push $image	|| exit -1
+
+# Docker image in a compressed format for CCRT to use by pcocc:
+save=$TRUST_ROOT/`basename $image.tar.gz`
 docker save $image | gzip > $save
 echo "$save created."
 
