9eaad5529ca2da38bee96b3453751c66a8e158e9
Pierre LEDAC <pierre.ledac@cea.fr>
2025-08-28 16:10:07 +0200

    [Info] Useless warning

--- src/Kernel/Framework/Discretiser.cpp
+++ src/Kernel/Framework/Discretiser.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2023, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -24,7 +24,7 @@ Implemente_instanciable(Discretiser,"Discretiser|Discretize",Interprete);
 // XD attr dis ref_discretisation_base dis 0 Name of the discretization object.
 
 
-int Discretiser::is_deja_appele=0;
+//int Discretiser::is_deja_appele=0;
 
 Sortie& Discretiser::printOn(Sortie& os) const { return Interprete::printOn(os); }
 
@@ -38,6 +38,7 @@ saisie :
   Objet_U& ob1=objet(nom1);
   Objet_U& ob2=objet(nom2);
 
+  /* PL: What is this warning ?
   if (is_deja_appele == 0) is_deja_appele = 1;
   else
     {
@@ -45,6 +46,7 @@ saisie :
       Cerr<<"there is probably an error in your data set"<<finl;
       // Process::exit();
     }
+    */
 
   if( !( sub_type(Discretisation_base, ob2) ) )
     {
