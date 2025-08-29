3ac0087cdae4a71913d6d141f1cad0f1e81f4339
Pierre LEDAC <pierre.ledac@cea.fr>
2025-08-29 09:30:47 +0200

    [cuDSS] Fix install if error

--- ThirdPart/src/LIBCUDSS/install.sh
+++ ThirdPart/src/LIBCUDSS/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 build_dir=libcudss-linux-`uname -m`-0.6.0.5_cuda12-archive
 mkdir -p $TRUST_ROOT/lib/src/LIBCUDSS
-tar -xf $TRUST_ROOT/externalpackages/cudss/$build_dir.tar.xz
+tar -xf $TRUST_ROOT/externalpackages/cudss/$build_dir.tar.xz || exit -1
 cp -r $build_dir/* $TRUST_ROOT/lib/src/LIBCUDSS
 rm -rf $build_dir
 echo "\$TRUST_ROOT/lib/src/LIBCUDSS/libcudss_static.a installed"
